b03657b77cf81dec7db6b997fca0c63e494a646d
SaadAhmadSaddiqui <mehunsaadi@gmail.com>
2019-11-06 03:44:02 +0400

    Initial Commit
    It includes all the work done so far. Including image tracking and parts of the floor popping up more and hiding them on touch using raycast.

---
+++ Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARCore;
+
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager trackedImageManager;
+
+    [SerializeField] private GameObject[] uniRooms;
+
+    public GameObject[] MyPlane
+    {
+        get { return uniRooms; }
+        set { uniRooms = value; }
+    }
+
+    [SerializeField] private Camera arCamera;
+    public Camera Camera
+    {
+        get { return arCamera; }
+        set { arCamera = value; }
+    }
+
+    Vector2 touchPosition = default;
+
+    private void Awake()
+    {
+        trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {   
+        trackedImageManager.trackedImagesChanged += OnImageChanged;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    public void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var imageTracked in args.added)
+        {
+            imageTracked.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        }
+    }
+
+    //private void Start()
+    //{
+    //    Vector3 bruh = uniRooms[0].transform.position;
+    //    Physics.Raycast(arCamera.ScreenPointToRay(bruh), out RaycastHit hit);
+    //    {
+    //        hit.collider.gameObject.GetComponent<Renderer>().enabled = false;
+    //    }
+        
+
+    //}
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            touchPosition = touch.position;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touchPosition);
+                if (Physics.Raycast(ray, out RaycastHit hitObject))
+                {
+                    if (hitObject.collider.gameObject.GetComponent<Renderer>().enabled == false)
+                    {
+                        hitObject.collider.gameObject.GetComponent<Renderer>().enabled = true;
+                    }
+                    else
+                    {
+                        hitObject.collider.gameObject.GetComponent<Renderer>().enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlacementController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlacementController : MonoBehaviour
+{
+    [SerializeField] private GameObject placedPrefab;
+
+    public GameObject PlacedPrefab
+    {
+        get { return placedPrefab; }
+        set { placedPrefab = value; }
+    }
+
+    private ARRaycastManager arRaycastManager;
+
+    public void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+    void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPostion))
+        {
+            return;
+        }
+
+        
+
+        if(arRaycastManager.Raycast(touchPostion, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+
+        }
+    }
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+}
